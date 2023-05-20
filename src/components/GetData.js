a7b8b766cb68ee184cc97a0496aba4386655bfae
Guru <guru28272@gmail.com>
2023-05-20 14:38:14 +0530

    first commit

---
+++ src/components/GetData.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+
+const GetData = ({ setFunc }) => {
+  // original url gives the cor's error, so i set it to 'proxy' in
+  // package.json dependencies
+  const url =
+    "api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const jsonData = await res.json();
+      //   console.log(jsonData);
+
+      setFunc(jsonData);
+    };
+    fetchData();
+  }, []);
+
+  //   optional way--- prop drilling
+
+  // const [data, setData] = useState();
+
+  //   return (
+  //     <div>
+  //       <TableRow data={data} />
+  //     </div>
+  //   );
+};
+export default GetData;
---
+++ src/components/TableRow.js
@@ -0,0 +1,22 @@
+const TableRow = ({ data }) => {
+  // console.log(data);
+  return (
+    <div>
+      {data &&
+        data.map((item) => {
+          const { id, name, image, current_price } = item;
+          return name || image || current_price ? (
+            <div key={id} className="box">
+              <h3>currenyName : {name}</h3>
+              <img src={image} alt="image" />
+              <h4>current_price : {current_price}</h4>
+            </div>
+          ) : (
+            <h2>no data found</h2>
+          );
+        })}
+      {!data && <h2>Loading...</h2>}
+    </div>
+  );
+};
+export default TableRow;
