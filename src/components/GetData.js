b17aec03b9293c6ba97ed95c53f147136b84ab08
Guru <guru28272@gmail.com>
2023-05-20 17:33:29 +0530

    url change

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,11 +1,8 @@
 import { useEffect } from "react";
 
 const GetData = ({ setFunc }) => {
-  // original url gives the cor's error, so i set it to 'proxy' in
-  // package.json dependencies
   const url =
-    "api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(url);
